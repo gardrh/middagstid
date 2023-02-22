9e3eb0ccfeae4b9656a6fe865b0a830672b4a0cb
gardrh <gardrh@gmail.com>
2023-02-22 20:40:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,19 @@ generateBtn.addEventListener("click", () => {
       const randomRow = data[randomIndex];
       const resultHTML = headers.map((header, index) => `<p><strong>${header}:</strong> ${randomRow[index]}</p>`).join("");
       resultDiv.innerHTML = resultHTML;
+    // Navigate to a new page with the result as a query parameter
+	window.location = `result.html?result=${result}`;
+}
     })
     .catch(error => console.error(error));
 });
+
+// Get the result element and parse the query string
+const resultDiv = document.querySelector(".result");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// Set the result text to the query parameter value
+resultDiv.innerHTML = urlParams.get("result");
 
 
