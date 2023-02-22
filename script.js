40e418fbdd018de169424abeddf3445949e7a46c
gardrh <gardrh@gmail.com>
2023-02-22 19:27:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ generateBtn.addEventListener("click", () => {
     })
     .catch(error => console.error(error));
 });
+
+generateBtn1.addEventListener("click", () => {
+  fetch("data.csv")
+    .then(response => response.text())
+    .then(text => {
+      const data = text.trim().split("\n").map(row => row.split(","));
+      const headers = data.shift();
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomRow = data[randomIndex];
+      const resultHTML = headers.map((header, index) => `<p><strong>${header}:</strong> ${randomRow[index]}</p>`).join("");
+      resultDiv.innerHTML = resultHTML;
+    })
+    .catch(error => console.error(error));
+})
