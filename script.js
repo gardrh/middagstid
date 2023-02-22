1e73b165dbf4de666792ded15b8559c1cc575375
gardrh <gardrh@gmail.com>
2023-02-22 19:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,21 +15,20 @@ generateBtn.addEventListener("click", () => {
     .catch(error => console.error(error));
 });
 
-const generateBtn1 = document.getElementById("generate-btn1");
+const generateBtn1 = document.getElementById("generate-btn");
 const resultDiv = document.querySelector(".result");
 
 generateBtn1.addEventListener("click", () => {
   fetch("data.csv")
-   .then(response => response.text())
+    .then(response => response.text())
     .then(text => {
-    const data = text.trim().split("\n").map(row => row.split(","));
- const headers = data.shift();
-     const randomIndex = Math.floor(Math.random() * data.length);
+      const data = text.trim().split("\n").map(row => row.split(","));
+      const headers = data.shift();
+      const randomIndex = Math.floor(Math.random() * data.length);
       const randomRow = data[randomIndex];
       const resultHTML = headers.map((header, index) => `<p><strong>${header}:</strong> ${randomRow[index]}</p>`).join("");
       resultDiv.innerHTML = resultHTML;
     })
     .catch(error => console.error(error));
 });
-
 
