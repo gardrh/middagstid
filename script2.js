62e79f2f90c6dd21a41b1baca48740d44a2c984b
gardrh <gardrh@gmail.com>
2023-02-22 20:03:29 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -8,19 +8,19 @@ const results = [
 	"Result 6",
 	"Result 7"
 ];
+const generateBtn = document.getElementById("generate-btn");
+const resultDiv = document.querySelector(".result");
 
-// Get the button element
-const generateBtn1 = document.getElementById("generate-btn1");
-
-// Define the generateResult function
-function generateResult() {
-	// Get a random index from the results array
-	const randomIndex = Math.floor(Math.random() * results.length);
-	// Get the result at the random index
-	const middag = results[randomIndex];
-	// Navigate to a new page with the result as a query parameter
-	window.location = `middag.html?result=${result}`;
-}
-
-// Attach the generateResult function to the button's click event
-generateBtn1.addEventListener("click", generateResult);
+generateBtn.addEventListener("click", () => {
+  fetch("data.csv")
+    .then(response => response.text())
+    .then(text => {
+      const data = text.trim().split("\n").map(row => row.split(","));
+      const headers = data.shift();
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomRow = data[randomIndex];
+      const resultHTML = headers.map((header, index) => `<p><strong>${header}:</strong> ${randomRow[index]}</p>`).join("");
+      resultDiv.innerHTML = resultHTML;
+    })
+    .catch(error => console.error(error));
+});
